9d9bc5b2cd1c559b56f6eae02baea4294ceefda4
zzf <1015317660@qq.com>
2018-01-17 20:33:15 +0800

    test

---
+++ src/main/java/com/jk/controller/Example.java
@@ -0,0 +1,16 @@
+package com.jk.controller;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/la")
+    public String home(){
+        return "hello world";
+    }
+}
